780f2f989bc7927f709cd4dff18ce3727096788c
Jacob Kraus <jacob.c.kraus@gmail.com>
2024-07-31 13:13:30 -0700

    implemented proof of concept for loading nested template

--- starfield_outpost_planner/outpost_planner_app/urls.py
+++ starfield_outpost_planner/outpost_planner_app/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/StarfieldOutpostPlanner/
-    path("", views.home, name="StarfieldOutpostPlanner$"),
-    # http://127.0.0.1:8000/StarfieldOutpostPlanner/?quantity=1&outpost_module=87
-    re_path(r"\?((quantity=\d\d?)&(outpost_module=\d\d?\d?))+",
-            views.outpost_modules_display,
-            name="outpost_modules_display"),
+    path("", views.home, name="StarfieldOutpostPlanner/$"),
     path("about", views.about, name="about"),
     path("contact", views.contact, name="contact"),
-    path("home", views.home, name="home")
+    path("home", views.home, name="home"),
 ]
--- starfield_outpost_planner/outpost_planner_app/views.py
+++ starfield_outpost_planner/outpost_planner_app/views.py
@@ -1,7 +1,6 @@
-from collections import OrderedDict
-from django.db import connection
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from .models import OutpostModule, Recipe
 
 
@@ -19,20 +18,36 @@ def contact(request):
 
 def home(request):
     outpost_modules = OutpostModule.objects.all()
-    recipes = Recipe.objects.all()
+    # TODO: get render object back from outpost_modules_display.html
+    #   pass it to render request for home.html
+    sub_view = outpost_modules_display(request,
+                                       num=int(request.GET['num']),
+                                       omID=int(request.GET['omID']),
+                                       outpost_modules=outpost_modules)
     return render(request,
                   "home.html",
-                  {"modules": outpost_modules, "recipes": recipes})
+                  {"modules": outpost_modules, "subView": sub_view})
 
 
-def outpost_modules_display(request, *args):
-    outpost_modules = OutpostModule.objects.all()
+def outpost_modules_display(request, num, omID, outpost_modules):
     recipes = Recipe.objects.all()
-    print("ARGS:")
-    for arg in args:
-        print("    " + repr(arg))
-    return render(request, "outpost_modules_display.html",
-                  {"modules": outpost_modules, "recipes": recipes})
+    module_query = outpost_modules.get(moduleID=omID)
+    # note: querying with .get() will throw an error if 0 or >1 results found
+    recipe_query = recipes.get(recipeID=module_query.recipeID)
+    if not recipe_query:
+        raise RuntimeError(f"query found no result for recipeID={recipe_query.recipeID}")
+    tally_required_resources = dict()
+    for attr in dir(recipe_query):
+        if not attr.startswith('required'):
+            continue
+        val = getattr(recipe_query, attr)
+        if not val:
+            continue
+        attr_key = attr[8:]  # cut off the "required" part of the label
+        tally_required_resources[attr_key] = num * int(val)
+    return render_to_string("outpost_modules_display.html",
+                            {"modules": outpost_modules, "recipes": recipes},
+                            request)
 
 
 def redirect_view(request):
