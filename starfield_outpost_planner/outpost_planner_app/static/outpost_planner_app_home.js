12ed1a2560ed9ddfc9264816c4c6ce563f95f25c
Jacob Kraus <jacob.c.kraus@gmail.com>
2024-08-05 14:39:53 -0700

    got add/remove rows working

--- starfield_outpost_planner/outpost_planner_app/static/outpost_planner_app_home.js
+++ starfield_outpost_planner/outpost_planner_app/static/outpost_planner_app_home.js
@@ -10,26 +10,22 @@ function deleteOutpostModuleLine(rowIndex)
     table.deleteRow(rowIndex);
 }
 
-/* failed approach
-function get(name){  // return get[name] || undefined (if var has no value or doesn't exist)
-   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      return decodeURIComponent(name[1]);
-}
-*/
-
 
 async function getRow(rowIndex)
 {
     // Reading the docs helped me get this function working correctly
     //  https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    // We want the id attribute values to have the rowIndex included so we can more easily
+    //  update the power and required items when the selected outpost module or count changes.
     const url = `outpostSelector${rowIndex}`;
     try {
-        const response = await fetch(url);
+        let response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
         try {
-            const htmlText = await response.text();
+            console.log(`response:\n${response}`)
+            let htmlText = await response.text();
             console.log(`getRow():\n${htmlText}`); // TODO: why is this good, but the return value not good?
             return htmlText;
         } catch (error) {
@@ -49,20 +45,11 @@ function outpostModuleSelectionChanged(id, moduleName, moduleCount)
 }
 
 
-function createElementFromHTML(htmlString) {
-  var div = document.createElement('div');
-  div.innerHTML = htmlString;
-  // Change this to div.childNodes to support multiple top-level nodes.
-  return div.firstChild;
-}
-
-
-function addOutpostModuleLine(rowIndex)
+async function addOutpostModuleLine(rowIndex)
 {
     // rowIndex will be the index in the table which the new line/row will inhabit.
-    const htmlText = getRow(rowIndex);
+    let htmlText = await getRow(rowIndex);
     console.log(`addOutpostModuleLine():\n${htmlText}`);
-    // TODO: why is htmlStr not the actual str? Why am I unable to get the value of my text??
     console.log("acquired row contents string");
     var table = document.getElementById("itemizationTable");
     var lastRow = document.getElementById(last_row_id);
@@ -71,12 +58,4 @@ function addOutpostModuleLine(rowIndex)
     var row = table.insertRow(rowIndex);
     console.log("added rowIndex = " + rowIndex);
     row.innerHTML = htmlText;
-
-    /*
-    // This approach didn't work
-    var fragment = createElementFromHTML(htmlStr);
-    // You can use native DOM methods to insert the fragment:
-    // document.body.insertBefore(fragment, document.body.childNodes[0]);
-    table.insertBefore(fragment, lastRow);
-    */
 }
